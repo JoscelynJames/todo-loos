8b801497e006d3dae351b23b28584d39378deac5
JoscelynJames <josce.james7@gmail.com>
2022-08-06 07:58:02 -0600

    added ant to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
+import {  createStyleImportPlugin,
+  AndDesignVueResolve, } from "vite-plugin-style-import";
 import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 import vue from "@vitejs/plugin-vue";
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,16 +16,23 @@ export default defineConfig({
     Components({
       resolvers: [AntDesignVueResolver()],
     }),
+    createStyleImportPlugin({
+      resolves: [ 
+        AndDesignVueResolve()
+      ]
+    })
   ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+      // "~": fileURLToPath(new URL("./node_modules", import.meta.url)),
+      '~' : path.resolve(__dirname, './node_modules'),
     },
   },
   css: {
     preprocessorOptions: {
       less: {
-        javascriptEnabled: true
+        javascriptEnabled: true,
       },
     },
   }
