b08c3ecd4486ac667aa143237782132b22d97a0c
JoscelynJames <josce.james7@gmail.com>
2022-08-07 14:22:11 -0600

    style updates

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
-<script setup lang="ts">
+<script lang="ts">
 import { RouterLink, RouterView } from "vue-router";
+import MainLayout from "@/layouts/MainLayout.vue";
 
+export default {
+  name: "App",
+  components: {
+    MainLayout,
+    RouterView
+  }
+};
 </script>
 
 <template>
+  <MainLayout />
   <RouterView />
 </template>
 
-<style scoped>
-
+<style lang="less">
+@import './styles/theme.less';
+@import './styles/global.less';
 </style>
